da18a303c2bba35e25f0fbb555d7a28fb5717cb2
fontesrp <fontesr2@gmail.com>
2016-12-30 10:05:42 -0200

    Add Xcode project files

---
+++ rubiks_cube/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  rubiks_cube
+//
+//  Created by Rodrigo Fontes on 30/12/16.
+//  Copyright © 2016 Rodrigo Fontes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
